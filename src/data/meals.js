4cccdab4b4fee19b98775b413e6a20c928ba7f9e
Samantha Salley <samanthasalley@gmail.com>
2019-11-29 17:12:23 -0800

    (create)   ❇️  base data files

---
+++ src/data/meals.js
@@ -0,0 +1,17 @@
+const mealOptions = {
+  breakfast: [
+    'Eggs',
+  ],
+  lunch: [
+    'Turkey Sandwich',
+  ],
+  dinner: [
+    'Chicken',
+  ],
+  snacks: [
+    'RX Bar',
+    'Other Bar',
+  ],
+};
+
+module.exports = mealOptions;
---
+++ src/data/workouts.js
@@ -0,0 +1,23 @@
+const workoutOptions = {
+  Boxing: [
+    'Box \'n Burn',
+    'Quiet Punch',
+  ],
+  Running: [
+    '1 mile',
+    '2 miles',
+    'Warmup Run - 0.5 miles',
+  ],
+  Climbing: [
+    'Bouldering',
+    'Top-Rope',
+  ],
+  Misc: [
+    'Class',
+    'Random HIIT',
+    'Home Exercises',
+    'Other',
+  ],
+};
+
+module.exports = workoutOptions;
