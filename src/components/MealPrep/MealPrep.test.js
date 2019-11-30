4e797312da896481ce5fc596e09efc32d9ff847b
Samantha Salley <samanthasalley@gmail.com>
2019-11-30 02:02:35 -0800

    (remove)  🔥   unnecessary MealPrep component

--- src/components/MealPrep/MealPrep.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MealPrep from './MealPrep';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MealPrep />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
