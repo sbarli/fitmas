45117e59233fe1234993e3f39af16111e29272e8
Samantha Salley <samanthasalley@gmail.com>
2019-11-30 02:05:42 -0800

    (update) App components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import styled from 'styled-components/macro';
+import moment from 'moment';
 // components
-// import FitmasCalendar from '../FitmasCalendar/FitmasCalendar';
-import MealForm from '../MealForm/MealForm';
+import Header from '../../libs/ui/Header/Header';
+import DayView from '../DayView/DayView';
+// import MealForm from '../MealForm/MealForm';
 
 // styled components
+import styled from 'styled-components/macro';
+
 const Wrapper = styled.div`
   height: 100vh;
+  width: 100%;
+  padding-left: 10rem;
+  padding-right: 10rem;
   background: steelblue;
   display: flex;
   flex-direction: column;
@@ -14,19 +20,83 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Title = styled.h1`
-  margin-top: 0;
-  padding-top: 5rem;
-`;
+const dummyData = {
+  Today: {
+    food: [
+      {
+        mealType: "Breakfast",
+        items: [
+          'Eggs',
+          'Bacon',
+          'Coffee',
+        ],
+      },
+      {
+        mealType: "Dinner",
+        items: [
+          'Grilled Chicken',
+          'Green Beans',
+          'Rice',
+        ],
+      },
+    ],
+    exercise: [
+      {
+        category: 'Running',
+        item: 'Warmup - 0.5 miles',
+      },
+      {
+        category: 'Boxing',
+        item: 'Quiet Punch',
+      },
+    ],
+  },
+  '2019-12-01': {
+    food: [
+      {
+        mealType: "Breakfast",
+        items: [
+          'Quiche',
+          'Coffee',
+        ],
+      },
+      {
+        mealType: "Snack",
+        items: [
+          'RX Bar',
+        ],
+      },
+    ],
+    exercise: [
+      {
+        category: 'Boxing',
+        item: 'Box \'n Burn',
+      },
+    ],
+  },
+};
 
 const App = () => {
   return (
     <Wrapper>
-      <header>
-        <Title>Welcome to 25 Days of Fitmas!</Title>
-      </header>
-      {/* <FitmasCalendar /> */}
-      <MealForm />
+      <Header
+        size="h1"
+        removeMarginTop
+        addPaddingTop>
+        <span role="img" aria-label="christmas-tree">🎄</span>
+        Welcome to 25 Days of Fitmas
+        <span role="img" aria-label="muscles">💪🏼</span>
+        !
+      </Header>
+      {/* <MealForm /> */}
+      <DayView
+        day="Today"
+        data={dummyData.Today}
+      />
+      <DayView
+        day={new Date(moment().add(1, 'd'))}
+        data={dummyData['2019-12-01']}
+      />
     </Wrapper>
   );
 };
