c9591a6caf8c296d11dc7fbe1c1c21d4e7b99888
Samantha Salley <samanthasalley@gmail.com>
2019-12-01 00:15:19 -0800

    (refactor)  ♻️  : (remove)  🔥  dummy data and (add)  ❇️  FitmasCalendar render

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,80 +1,25 @@
 import React from 'react';
 import moment from 'moment';
+import 'moment-timezone';
 // components
 import Header from '../../libs/ui/Header/Header';
 import DayView from '../DayView/DayView';
-// import MealForm from '../MealForm/MealForm';
+import FitmasCalendar from '../FitmasCalendar/FitmasCalendar';
+// assets
+import dummyData from '../../data/dummy';
 
 // styled components
 import styled from 'styled-components/macro';
 
 const Wrapper = styled.div`
-  height: 100vh;
   width: 100%;
   padding-left: 10rem;
   padding-right: 10rem;
-  background: steelblue;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
 `;
-
-const dummyData = {
-  Today: {
-    food: [
-      {
-        mealType: "Breakfast",
-        items: [
-          'Eggs',
-          'Bacon',
-          'Coffee',
-        ],
-      },
-      {
-        mealType: "Dinner",
-        items: [
-          'Grilled Chicken',
-          'Green Beans',
-          'Rice',
-        ],
-      },
-    ],
-    exercise: [
-      {
-        category: 'Running',
-        item: 'Warmup - 0.5 miles',
-      },
-      {
-        category: 'Boxing',
-        item: 'Quiet Punch',
-      },
-    ],
-  },
-  '2019-12-01': {
-    food: [
-      {
-        mealType: "Breakfast",
-        items: [
-          'Quiche',
-          'Coffee',
-        ],
-      },
-      {
-        mealType: "Snack",
-        items: [
-          'RX Bar',
-        ],
-      },
-    ],
-    exercise: [
-      {
-        category: 'Boxing',
-        item: 'Box \'n Burn',
-      },
-    ],
-  },
-};
 
 const App = () => {
   return (
@@ -84,19 +29,15 @@ const App = () => {
         removeMarginTop
         addPaddingTop>
         <span role="img" aria-label="christmas-tree">🎄</span>
-        Welcome to 25 Days of Fitmas
+        Welcome to 25 Days of Fitmas, {dummyData.user}
         <span role="img" aria-label="muscles">💪🏼</span>
         !
       </Header>
-      {/* <MealForm /> */}
       <DayView
         day="Today"
-        data={dummyData.Today}
+        data={dummyData.plan[moment().tz(moment.tz.guess()).format('YYYY-MM-DD')]}
       />
-      <DayView
-        day={new Date(moment().add(1, 'd'))}
-        data={dummyData['2019-12-01']}
-      />
+      <FitmasCalendar />
     </Wrapper>
   );
 };
