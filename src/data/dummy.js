1af1f18da9a88c3e92f363df097faf1dda3a2958
Samantha Salley <samanthasalley@gmail.com>
2019-12-01 00:11:28 -0800

    (move)  🚚  dummy data to single data file

---
+++ src/data/dummy.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "user": "tumbaa",
+  "plan": {
+    "2019-12-01": {
+      "food": {
+        "Breakfast": {
+          "items": [
+            "Smoothie",
+          ],
+          "done": true,
+        },
+        "Dinner": {
+          "items": [
+            "Chicken",
+            "Green Beans",
+          ],
+          "done": false,
+        },
+      },
+      "exercise": [
+        {
+          "category": "Running",
+          "item": "Warmup: 0.5 miles",
+          "done": false,
+        },
+        {
+          "category": "Boxing",
+          "item": "Quiet Punch",
+          "done": false,
+        },
+      ],
+    }
+  }
+};
