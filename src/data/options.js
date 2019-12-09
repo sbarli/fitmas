553f4c6d8e22aac6743af977f447b7d1975bdad5
Samantha Salley <samanthasalley@gmail.com>
2019-12-09 11:06:19 -0800

    (create)  ❇️  basic selection options for plan items

---
+++ src/data/options.js
@@ -0,0 +1,9 @@
+module.exports = {
+  types: ['food', 'exercise'],
+  food: {
+    categories: ['Breakfast', 'Lunch', 'Dinner', 'Snacks'],
+  },
+  exercise: {
+    categories: ['Boxing', 'Running', 'Climbing', 'Misc'],
+  },
+};
